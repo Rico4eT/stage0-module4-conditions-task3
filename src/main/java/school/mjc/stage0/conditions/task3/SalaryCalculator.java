1ac5ca115f037e0fb9e7488431dca247c9f8e73f
Rico4eT <shiwawa96@mail.ru>
2024-01-21 17:47:06 +0500

    My First Multuple Conditions

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,20 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if (salary <= 10000) {
+            float tax = (float) 0.15;
+            float salaryAfterTax = salary - salary * tax;
+            System.out.println(salaryAfterTax);
+        } else if (salary <= 20000) {
+            float tax = (float) 0.18;
+            float salaryAfterTax = salary - salary * tax;
+            System.out.println(salaryAfterTax);
+        } else if (salary > 20000) {
+            float tax = (float) 0.20;
+            float salaryAfterTax = salary - salary * tax;
+            System.out.println(salaryAfterTax);
+        } else {
+            System.out.println("wrong input!");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -2,6 +2,16 @@ package school.mjc.stage0.conditions.task3;
 
 public class Seasons {
     public void tellTheSeasonByMonthNumber(int month) {
-
+        if (month > 0 && month < 4) {
+            System.out.println("Winter");
+        } else if (month >= 4 && month < 6) {
+            System.out.println("Spring");
+        } else if (month >= 6 && month < 9) {
+            System.out.println("Summer");
+        } else if (month >= 9 && month < 13) {
+            System.out.println("Autumn");
+        } else {
+            System.out.println("wrong number!");
+        }
     }
 }
